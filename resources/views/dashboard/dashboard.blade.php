2aa0c89761512a578db6894a9c02496f31c8283a
= <=>
2017-03-13 00:52:50 -0400

    dashboards design

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -24,14 +24,50 @@
             </div>
         </div>
         <div class="col-md-6">
-            <div class="panel panel-primary">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Graph Title</h3>
-                </div>
-                <div class="panel-body">
-                    this where the graph will go
-                </div>
-            </div>
+            <ul class="list-unstyled">
+                <li>
+                    <div class="panel panel-primary h-225 overflow-scroll">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Top Performers - Invoice Week of {{date('M jS', strtotime('last wednesday'))}}</h3>
+                        </div>
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th class="w-80"># of Sales</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>Bret Payment</td>
+                                <td class="text-center">15</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </li>
+                <li>
+                    <div class="panel panel-danger h-225 overflow-scroll">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Underperformers</h3>
+                        </div>
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th class="w-80"># of Sales</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>Drew Payment</td>
+                                <td class="text-center">-4</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </li>
+            </ul>
         </div>
     </div>
 
