9d34ff71e405479213bf6ec099f86ffb957fb223
= <=>
2017-04-09 16:11:14 -0400

    troubleshooting date format issue on show-invoice

--- resources/views/invoices/_editinvoice.blade.php
+++ resources/views/invoices/_editinvoice.blade.php
@@ -12,7 +12,7 @@
                 <h4 id="issueDate" data-date="{{$invoiceDate}}">Issued: {{$issueDate}}</h4>
             </li>
             <li>
-                <h4 id="wkendDate" data-weekending="{{$weekEnding}}">Weekending: {{date_format(date_create($weekEnding), "F jS, Y")}}</h4>
+                <h4 id="wkendDate" data-weekending="{{$weekEnding}}">Weekending: {{date_format(new DateTime($weekEnding), "F jS, Y")}}</h4>
             </li>
         </ul>
     </div>
