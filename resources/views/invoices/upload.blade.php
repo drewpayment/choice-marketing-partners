94cffa9bf29582db53e71fb93f17c97024e857e9
= <=>
2017-08-01 00:14:17 -0400

    redesigning invoice upload form and rewriting validation to handle more scenarios

--- resources/views/invoices/upload.blade.php
+++ resources/views/invoices/upload.blade.php
@@ -16,17 +16,28 @@
 
 <div class="row pt-10">
 	<div class="col-xs-8">
-		<h2>Upload a New Invoice <small>one upload per employee</small></h2>
+		<h2>New Invoice</h2>
+	</div>
+	<div class="col-xs-4 pull-right">
+		<button class="btn btn-default" id="addOverrides"><i class="fa fa-plus"></i> Overrides</button>
+		<button class="btn btn-default" id="addExpenses"><i class="fa fa-plus"></i> Expenses</button>
 	</div>
 </div>
 
-<div class="row pt-20">
+<div class="row pt-20 pb-10">
 	<div class="col-xs-12">
-		<ul class="list-inline">
-			<li>
-				<label for="employee"><h4>Agent: </h4></label>
-			</li>
-			<li>
+		<div class="form-inline">
+			<div class="form-group">
+				<label for="vendor"></label>
+				<select class="selectpicker" id="vendor" data-mobile="true">
+					<option value="-1" selected>Select Vendor</option>
+					@foreach($vendors as $v)
+						<option value="{{$v->id}}">{{$v->name}}</option>
+					@endforeach
+				</select>
+			</div>
+			<div class="form-group">
+				<label for="employee">Agent: </label>
 				<select class="selectpicker" id="employee" data-live-search="true">
 					<?php $i = 0; ?>
 					@foreach($emps as $emp)
@@ -34,11 +45,9 @@
 						<?php $i++; ?>
 					@endforeach
 				</select>
-			</li>
-			<li>
-				<span><h4>Issue Date: </h4></span>
-			</li>
-			<li>
+			</div>
+			<div class="form-group">
+				<label for="issueDate">Issued: </label>
 				<select class="selectpicker" id="issueDate" data-mobile="true">
 					<?php $n = 0; ?>
 					<option value="-1" selected>Pick One</option>
@@ -47,26 +56,12 @@
 						<?php $n++; ?>
 					@endforeach
 				</select>
-			</li>
-			<li>
-				<span><h4>Weekending Date: </h4></span>
-			</li>
-			<li>
-				<input type="text" class="form-control datepicker-hot" id="wkendDate"></input>
-			</li>
-			<br>
-			<li>
-				<span><h4>Vendor: </h4></span>
-			</li>
-			<li>
-				<select class="selectpicker" id="vendor" data-mobile="true">
-					<option value="-1" selected>Select Vendor</option>
-					@foreach($vendors as $v)
-						<option value="{{$v->id}}">{{$v->name}}</option>
-					@endforeach
-				</select>
-			</li>
-		</ul>
+			</div>
+			<div class="form-group">
+				<label for="wkendDate">Wkending: </label>
+				<input type="text" class="form-control datepicker-hot" id="wkendDate">
+			</div>
+		</div>
 	</div>
 </div>
 
@@ -79,26 +74,21 @@
 <div class="row pt-10">
 	<div class="col-xs-12">
 		<ul class="list-inline">
-			<li>
-				<h2>Overrides <small>Enter any applicable overrides.</small></h2>
-				<br>
+			<li class="hidden" id="overrides">
+				<h3>Overrides</h3>
 				<div id="overridesTable" class="overridesTable" data-parent="true"></div>
 			</li>
-			<li>
-				<h2>Expenses <small>Enter any applicable expenses.</small></h2>
-				<br>
+			<li class="hidden" id="expenses">
+				<h3>Expenses</h3>
 				<div id="expensesTable" class="overridesTable" data-parent="true"></div>
 			</li>
 		</ul>
-		<div class="pull-right pr-60">
-			<ul class="list-inline">
-				<li>
-					<button class="btn btn-primary" data-tag="1" data-vero="button">Submit</button>
-				</li>
-				<li>
-					<button class="btn btn-default" data-vero="button">Cancel</button>
-				</li>
-			</ul>
+	</div>
+</div>
+<div class="row pt-20">
+	<div class="col-xs-11">
+		<div class="pull-right">
+			<button class="btn btn-primary" data-tag="1" data-vero="button"><i class="fa fa-save"></i> Save</button>
 		</div>
 	</div>
 </div>
@@ -111,6 +101,26 @@
 <script type="text/javascript">
 $(function(){
 	$('#wkendDate').datepicker();
+
+	$(document).on('click', '#addOverrides', function(){
+	    var el = $(this);
+	    if(el.find('i').hasClass('fa-plus')){
+            el.find('i').removeClass('fa-plus').addClass('fa-minus');
+		} else {
+	        el.find('i').removeClass('fa-minus').addClass('fa-plus');
+		}
+	   	$('#overrides').toggleClass('hidden');
+	});
+
+	$(document).on('click', '#addExpenses', function(){
+        var el = $(this);
+        if(el.find('i').hasClass('fa-plus')){
+            el.find('i').removeClass('fa-plus').addClass('fa-minus');
+        } else {
+            el.find('i').removeClass('fa-minus').addClass('fa-plus');
+        }
+	   	$('#expenses').toggleClass('hidden');
+	});
 });
 
 $(document).on('click', 'button', handleClick);
@@ -122,10 +132,10 @@ var paystubHot = new Handsontable(paystubContainer, {
 	minCols: 6,
 	rowHeaders: true,
 	colHeaders: [
-		'ID', 'Sale Date', 'First Name', 'Last Name', 'Address', 'City', 'Sale Status', 'Amount'
+		'Sale Date', 'First Name', 'Last Name', 'Address', 'City', 'Sale Status', 'Amount'
 	],
 	colWidths: [
-		40, 120, 140, 160, 220, 150, 100, 100
+		120, 140, 160, 220, 150, 100, 100
 	],
 	contextMenu: true,
 	allowInsertColumn: false,
@@ -133,7 +143,6 @@ var paystubHot = new Handsontable(paystubContainer, {
 	minSpareRows: 1,
 	data: [],
 	dataSchema: {
-		id: null, 
 		saleDate: null,
 		custName: { first: null, last: null },
 		address: null,
@@ -142,7 +151,6 @@ var paystubHot = new Handsontable(paystubContainer, {
 		amount: null
 	},
 	columns: [
-		{data: 'id'},
 		{data: 'saleDate', type: 'date', dateFormat: 'MM/DD/YYYY'},
 		{data: 'custName.first'},
 		{data: 'custName.last'},
@@ -158,26 +166,45 @@ var overHot = new Handsontable(overrideContainer, {
     minRows: 3,
     maxRows: 15,
     rowHeaders: true,
-    colHeaders: ['ID', 'Name', '# of Sales', 'Commission', 'Total'],
-    colWidths: ['40', '140', '100', '120', '100'],
+    colHeaders: ['Name', '# of Sales', 'Commission', 'Total'],
+    colWidths: ['140', '100', '120', '100'],
     contextMenu: true,
     allowInsertColumn: false,
     allowRemoveColumn: false,
     minSpareRows: 1,
     data: [],
     dataSchema: {
-        id: null,
         name: null,
         sales: null,
         commission: null,
         total: null
     },
     columns: [
-        {data: 'id', editor: false},
         {data: 'name'},
-        {data: 'sales'},
-        {data: 'commission'},
-        {data: 'total'}
+        {
+            data: 'sales',
+			className: 'htRight'
+		},
+        {
+            data: 'commission',
+			className: 'htRight',
+			type: 'numeric',
+			format: '0.00'
+		},
+        {
+            data: 'total',
+			renderer: function(hot, td, row, col, prop, value, cellProperties){
+                var sales = hot.getDataAtCell(row, 1);
+                var comm = hot.getDataAtCell(row, 2);
+
+                if(sales !== undefined && comm !== undefined && sales > 0 && comm > 0){
+                    var result = sales * comm;
+                    td.innerHTML = '<span class="pull-right">$'+ result.toFixed(2) +'</span>';
+				} else {
+                    td.innerHTML = '<span class="pull-right"></span>';
+				}
+			}
+        }
     ]
 });
 
@@ -200,7 +227,12 @@ var expHot = new Handsontable(expenseContainer, {
     },
     columns: [
         {data: 'type'},
-        {data: 'amount'},
+        {
+            data: 'amount',
+			className: 'htRight',
+			type: 'numeric',
+			format: '0.00'
+		},
         {data: 'notes'}
     ]
 });
