1e89cad0c1c4321cd6705b98f7602348b8dff9ee
Drew Payment <drew@verostack.io>
2017-11-03 23:27:47 -0400

    Blog layout issues
    Fixed layouts that weren't actually using new template correctly.

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -1,4 +1,4 @@
-@extends('blog.layout', ['container' => 'container-fluid'])
+@extends('blog.layout', ['container' => 'container-fluid', 'useWrapper' => true])
 
 @section('blog_title', '<i class="fa fa-pencil"></i> Compose New Entry')
 
@@ -13,48 +13,16 @@
         });
     </script>
 
-    <div class="row">
-        <div class="col-md-2">
-            <div class="box box-default">
-                <div class="box-content">
-                    <ul class="nav nav-pills nav-stacked">
-                        <li>
-                            <a href="{{url('/blog')}}"><i class="fa fa-home"></i> All Posts</a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="{{url('/blog/user/'.Auth::user()->id)}}"><i class="fa fa-user"></i> Profile</a>
-                        </li>
-                        <li>
-                            <a href="{{url('/blog/new-post')}}"><i class="fa fa-pencil"></i> Compose</a>
-                        </li>
-                        <li>
-                            <a href="{{url('/blog/my-all-posts')}}"><i class="fa fa-th-list"></i> My Posts</a>
-                        </li>
-                        <li>
-                            <a href="{{url('/blog/my-drafts')}}"><i class="fa fa-clipboard"></i> My Drafts</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+    <form action="{{url('/blog/new-post')}}" method="POST">
+        <input type="hidden" name="_token" value="{{csrf_token()}}">
+        <div class="form-group">
+            <input value="{{old('title')}}" placeholder="Enter title here" type="text" name="title" class="form-control" required />
         </div>
-        <div class="col-md-10">
-            <div class="box box-default">
-                <div class="box-content">
-                    <form action="{{url('/blog/new-post')}}" method="POST">
-                        <input type="hidden" name="_token" value="{{csrf_token()}}">
-                        <div class="form-group">
-                            <input value="{{old('title')}}" placeholder="Enter title here" type="text" name="title" class="form-control" required />
-                        </div>
-                        <div class="form-group">
-                            <textarea name="body" id="body" style="visibility: hidden;">{{old('body')}}</textarea>
-                        </div>
-                        <input type="submit" name="publish" class="btn btn-success" value="Publish" />
-                        <input type="submit" name="save" class="btn btn-default" value="Save Draft" />
-                    </form>
-                </div>
-            </div>
+        <div class="form-group">
+            <textarea name="body" id="body" style="visibility: hidden;">{{old('body')}}</textarea>
         </div>
-    </div>
+        <input type="submit" name="publish" class="btn btn-success" value="Publish" />
+        <input type="submit" name="save" class="btn btn-default" value="Save Draft" />
+    </form>
 
 @endsection
--- resources/views/blog/layout.blade.php
+++ resources/views/blog/layout.blade.php
@@ -22,50 +22,53 @@
                         @yield('blog-content')
                     </div>
                 @else
-                    <div class="row">
-                        @if(Auth::check())
-                            <div class="col-md-2">
-                                <div class="box box-default">
-                                    <div class="box-content">
-                                        <ul class="nav nav-pills nav-stacked">
-                                            <li>
-                                                <a href="{{url('/blog')}}"><i class="fa fa-home"></i> All Posts</a>
-                                            </li>
-                                            <li class="divider"></li>
-                                            <li>
-                                                <a href="{{url('/blog/user/'.Auth::user()->id)}}"><i class="fa fa-user"></i> Profile</a>
-                                            </li>
-                                            <li>
-                                                <a href="{{url('/blog/new-post')}}"><i class="fa fa-pencil"></i> Compose</a>
-                                            </li>
-                                            <li>
-                                                <a href="{{route('my-all-posts')}}"><i class="fa fa-th-list"></i> My Posts</a>
-                                            </li>
-                                            <li>
-                                                <a href="{{url('/blog/my-drafts')}}"><i class="fa fa-clipboard"></i> My Drafts</a>
-                                            </li>
-                                            @if(Auth::check() && Auth::user()->is_admin())
+                    <div class="box-content">
+                        <div class="row">
+                            @if(Auth::check())
+                                <div class="col-md-2">
+                                    <div class="box box-default">
+                                        <div class="box-content">
+                                            <ul class="nav nav-pills nav-stacked">
                                                 <li>
-                                                    <a href="{{url('/blog/comment-approvals')}}"><i class="fa fa-thumbs-o-up"></i> Approve Comments</a>
+                                                    <a href="{{url('/blog')}}"><i class="fa fa-home"></i> All Posts</a>
                                                 </li>
-                                            @endif
-                                        </ul>
+                                                <li class="divider"></li>
+                                                <li>
+                                                    <a href="{{url('/blog/user/'.Auth::user()->id)}}"><i class="fa fa-user"></i> Profile</a>
+                                                </li>
+                                                <li>
+                                                    <a href="{{url('/blog/new-post')}}"><i class="fa fa-pencil"></i> Compose</a>
+                                                </li>
+                                                <li>
+                                                    <a href="{{route('my-all-posts')}}"><i class="fa fa-th-list"></i> My Posts</a>
+                                                </li>
+                                                <li>
+                                                    <a href="{{url('/blog/my-drafts')}}"><i class="fa fa-clipboard"></i> My Drafts</a>
+                                                </li>
+                                                @if(Auth::check() && Auth::user()->is_admin())
+                                                    <li>
+                                                        <a href="{{url('/blog/comment-approvals')}}"><i class="fa fa-thumbs-o-up"></i> Approve Comments</a>
+                                                    </li>
+                                                @endif
+                                            </ul>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        @else
-                            <div class="col-md-2">
-                                <div class="box box-default">
-                                    <div class="box-content">
-                                        <a href="{{url('/login')}}" class="btn btn-link">Login to access all features</a>
+                            @else
+                                <div class="col-md-2">
+                                    <div class="box box-default">
+                                        <div class="box-content">
+                                            <a href="{{url('/login')}}" class="btn btn-link">Login to access all features</a>
+                                        </div>
                                     </div>
                                 </div>
+                            @endif
+                            <div class="col-md-10">
+                                @yield('blog-content')
                             </div>
-                        @endif
-                        <div class="col-md-10">
-                            @yield('blog-content')
                         </div>
                     </div>
+
                 @endif
             </div>
         </div>
