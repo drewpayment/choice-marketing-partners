482230bba104a0a3840099f7365415fa9f578df6
Andrew Payment <drew.payment@gmail.com>
2018-02-13 13:54:31 -0500

    hotfix

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -543,12 +543,8 @@ class InvoiceController extends Controller
 				{
 					$issueDate = Carbon::createFromFormat('Y-m-d', $issueDate, 'America/Detroit');
 					$nextWednesday = new Carbon('next wednesday');
-					if(($key = array_search($issueDate, (array)$issueDates)) != false)
-					{
-						if($issueDate > $nextWednesday) $issueDates = $issueDates->slice(1);
-					}
+					if($issueDate > $nextWednesday) $issueDates = $issueDates->slice(1);
 				}
-				$issueDates = array_values((array)$issueDates);
 
 				$today = Carbon::now()->tz('America/Detroit');
 				$nextIssue = Carbon::createFromFormat('Y-m-d', $issueDates[0], 'America/Detroit');
