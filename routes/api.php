232873446ebd5e72bf5f6c43749ba8562edfd8ad
Drew Payment <drew@verostack.io>
2020-02-20 00:45:53 -0500

    partial

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/account/user-info', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+// Route::get('/account/user-info', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:api');
