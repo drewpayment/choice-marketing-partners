853eb13f8c268186f1f961cf9da67dc109e63cff
drewpayment <drew.payment@gmail.com>
2021-11-27 13:34:23 -0500

    wip

--- cmp/src/environments/environment.prod.ts
+++ cmp/src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
+  api: '/api',
   bugsnag: '541f3417fe6392a170dcb356e728e7fd',
+  zipcode: 'dbd21640-36c1-11ec-8553-2d4d26a369c6',
+  overrideZipcodeDevelopment: true,
 };
