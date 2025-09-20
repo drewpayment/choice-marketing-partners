511a364df6aecefd9aac0c14b206337aea2234b0
drewpayment <drew.payment@gmail.com>
2025-09-20 14:24:44 -0400

    feat(choice-marketing-partners): integrate Vercel Speed Insights
    This commit integrates the Vercel Speed Insights tool to
    monitor and improve the performance of the application.
    The `@vercel/speed-insights` package is added and the
    SpeedInsights component is included in the layout.

--- choice-marketing-partners/src/app/layout.tsx
+++ choice-marketing-partners/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/components/providers";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <Providers>
           {children}
         </Providers>
+        <SpeedInsights/>
       </body>
     </html>
   );
